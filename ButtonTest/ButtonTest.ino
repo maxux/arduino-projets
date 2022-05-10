4dcd7bcf4ffe0728c703d57be37789c5d70a3c7a
Maxime Daniel <root@maxux.net>
2022-05-10 17:18:19 +0200

    debug: update button test

--- ButtonTest/ButtonTest.ino
+++ ButtonTest/ButtonTest.ino
@@ -1,11 +1,52 @@
+
+int coolup = 0;
+int cooldown = 0;
+int lastup = LOW;
+int lastdown = LOW;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+}
+
+void action_up(int up) {
+  if(coolup > millis() - 1000)
+    return;
+      
+  if(lastup == LOW) {
+    Serial.println("UP Pressed !");
+  }
+
+  lastup = up;
+  coolup = millis(); 
+}
+
+void action_down(int down) {
+  if(cooldown > millis() - 1000)
+    return;
+
+  if(lastdown == LOW) {
+    Serial.println("DOWN Pressed !");
+  }
+  
+  lastdown = down;
+  cooldown = millis();
 }
 
 void loop() {
-  int but = digitalRead(2);
-  Serial.println(but);
+  int up = digitalRead(2);
+  int down = digitalRead(3);
 
-  delay(200);
+  if(lastup != up) {
+    action_up(up);
+  }
+
+  if(lastdown != down) {
+    action_down(down);
+  }
+
+  // Serial.print(up);
+  // Serial.println(down);
+  delay(50);
 }
