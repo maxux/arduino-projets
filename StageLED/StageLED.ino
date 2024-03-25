ce6d5c6d880ff920ed2699f223ca23faad0e4094
Maxime Daniel <root@maxux.net>
2024-03-25 06:37:02 +0100

    stageled: add waiting network debug colors

--- StageLED/StageLED.ino
+++ StageLED/StageLED.ino
@@ -17,10 +17,14 @@ using namespace qindesign::network;
 EthernetUDP udp(32);
 
 #define STRIPE_PIN   1
-#define PER_LANE     (12 * 120)
+#define SEG_PER_LANE 12
+#define LED_PER_SEG  120
+#define PER_LANE     (SEG_PER_LANE * LED_PER_SEG)
 #define NUM_LANES    2
 #define TOTAL_LEDS   (NUM_LANES * PER_LANE)
-#define BRIGHTNESS   80
+
+bool netstate = false;
+bool linkinit = false;
 
 CRGB leds[TOTAL_LEDS];
 server_stats_t mainstats = {
@@ -34,52 +38,37 @@ server_stats_t mainstats = {
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("[+] initializing stage-led controler");
 
   // Ethernet.setDHCPEnabled(false);
   Ethernet.setHostname("ledstage");
   Ethernet.begin();
+  Serial.println("[+] network initialized");
   
   FastLED.setDither(0);
   FastLED.addLeds<NUM_LANES, WS2811, STRIPE_PIN, RGB>(leds, PER_LANE);
   FastLED.setMaxRefreshRate(30);
+  Serial.println("[+] led initialized");
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // initializing light
+  // initializing default light
   for(int i = 0; i < TOTAL_LEDS; i++)
-    leds[i] = CHSV(200, 255, 50);
-  
-  #if 0
-  for(int i = 0; i < TOTAL_LEDS; i++) {
-    /*
-    if(i > 800 && i < 1440) {
-      leds[i] = CHSV(100, 255, BRIGHTNESS / 2);
-      continue;
-    }
-    
-    if(i > 2000) {
-      leds[i] = CHSV(100, 255, BRIGHTNESS / 2);
-      continue;
-    }
-    */
-
-    leds[i] = CHSV(0, 0, 0);
-  }
-  #endif
+    leds[i] = CHSV(200, 255, 10);
 
   FastLED.show();
 
   udp.beginWithReuse(1111);
   mainstats.state = 1;
 
-  Serial.println("[+] server listening");
+  Serial.println("[+] server listening (port 1111)");
 }
 
 void ethernet_status() {
   uint8_t mac[6];
   Ethernet.macAddress(mac);
 
-  // Serial.printf("[+] mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.printf("[+] mac address: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
   bool state = Ethernet.linkState();
   int speed = Ethernet.linkSpeed();
@@ -92,26 +81,15 @@ void ethernet_status() {
   Serial.println(" Mbps");
 }
 
-char base = 0;
 int received = 0;
-int lastcheck = 0;
-
-void loopx() {
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  char value = base;
-  
-  for(int i = 0; i < TOTAL_LEDS; i++) {
-    leds[i] = CHSV(value++, 255, BRIGHTNESS);
-  }
-
-  FastLED.show();
-  digitalWrite(LED_BUILTIN, LOW);
-
-  base += 1;
-}
+uint32_t lastcheck = 0;
 
 void loop() {
+  bool linkstate = Ethernet.linkState();
+  if(!linkstate && !linkinit) {
+    waiting_network();
+  }
+
   int packetsize = udp.parsePacket();
 
   if(packetsize >= 0) {
@@ -126,7 +104,7 @@ void loop() {
 
     digitalWrite(LED_BUILTIN, HIGH);
 
-    uint8_t *data = udp.data();
+    uint8_t *data = (uint8_t *) udp.data();
     int led = 0;
 
     for(int i = 0; i < packetsize; i += 3) {
@@ -146,14 +124,47 @@ void loop() {
   }
 
   if(millis() > lastcheck + 1000) {
-    // ethernet_status();
-    
     mainstats.time_current = millis();
     mainstats.fps = mainstats.frames - mainstats.old_frames;
 
-    udp.send("10.241.0.51", 1111, (char *) &mainstats, sizeof(mainstats));
+    // FIXME
+    udp.send("10.241.0.51", 1111, (uint8_t *) &mainstats, sizeof(mainstats));
 
     lastcheck = millis();
     mainstats.old_frames = mainstats.frames;
+  }
+}
+
+//
+// no network debug state
+//
+char base = 0;
+uint32_t lastnetcheck = 0;
+
+void waiting_network() {
+  int stripes = NUM_LANES * SEG_PER_LANE;
+  char value = base;
+
+  for(int i = 0; i < TOTAL_LEDS; i++)
+    leds[i] = CRGB(0, 0, 0);
+
+  for(int i = 0; i < stripes; i++) {
+    int index = i * LED_PER_SEG;
+
+    for(int stripe = 0; stripe <= i; stripe++) {
+      leds[index + stripe] = CHSV(value++, 255, 10);
+    }
+  }
+
+  base += 1;
+
+  FastLED.show();
+  FastLED.delay(100);
+
+  Serial.println("[+] sending debug colors, waiting for network");
+
+  if(millis() > lastnetcheck + 10000) {
+    lastnetcheck = millis();
+    Serial.printf("[+] still waiting for network link [uptime: %u sec]\n", millis() / 1000);
   }
 }
